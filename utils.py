317a4279c0cf4ea884c2161080768e62ae4e61a4
monitg <142651228+monitg@users.noreply.github.com>
2024-02-04 15:53:59 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -612,24 +612,35 @@ async def verify_user(bot, userid, token):
     tz = pytz.timezone('Asia/Kolkata')
     today = date.today()
     VERIFIED[user.id] = str(today)
-
+    
 async def check_verification(bot, userid):
-    user = await bot.get_users(userid)
+    user = await bot.get_users(int(userid))
     if not await db.is_user_exist(user.id):
         await db.add_user(user.id, user.first_name)
         await bot.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(user.id, user.mention))
     tz = pytz.timezone('Asia/Kolkata')
-    today = time.today()
-    if user.id in VERIFIED.keys():
-        EXP = VERIFIED[user.id]
-        hours,mintues,seconds = EXP.split('-')
-        comp = time(int(hours), int(minutes), int(seconds))
-        if comp<today:
-            return False
+    today = date.today()
+    now = datetime.now(tz)
+    curr_time = now.strftime("%H:%M:%S")
+    hour1, minute1, second1 = curr_time.split(":")
+    curr_time = time(int(hour1), int(minute1), int(second1))
+    status = await get_verify_status(user.id)
+    date_var = status["date"]
+    time_var = status["time"]
+    years, month, day = date_var.split('-')
+    comp_date = date(int(years), int(month), int(day))
+    hour, minute, second = time_var.split(":")
+    comp_time = time(int(hour), int(minute), int(second))
+    if comp_date<today:
+        return False
+    else:
+        if comp_date == today:
+            if comp_time<curr_time:
+                return False
+            else:
+                return True
         else:
             return True
-    else:
-        return False
     
     
 async def send_all(bot, userid, files, ident, chat_id, user_name, query):
