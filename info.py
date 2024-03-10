035d4417f7d930eb7e1969ff1100468a1d636167
Kamaleshmlp <126389754+Kamaleshmlp@users.noreply.github.com>
2024-03-10 11:51:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://monitgmoni:MoniTG007@cluster0.bxwiejd.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Kamalesh:KamaleshMLP7@cluster7.nn2x4jt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster7")
 DATABASE_NAME = environ.get('DATABASE_NAME', "auto-search")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
