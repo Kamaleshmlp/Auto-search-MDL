87f7be4c3ade403a1986ed291827d222aabb08e7
Kamaleshmlp <126389754+Kamaleshmlp@users.noreply.github.com>
2024-06-15 09:13:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,7 +28,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/30dd0afade9204d986
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5983189506').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '1002053727982').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '1001912307994').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
@@ -48,17 +48,17 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "auto-search")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'bindaaslinks.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'e6ddf757f3eebd1f564d9d78c2264ee1422e8f12')
+VERIFY = bool(environ.get('VERIFY', True))
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'Modijiurl.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '384a1b6a599b42aec68078df1a53c8143b01d74d')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "6")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 PORT = environ.get("PORT", "8080")
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+8j2H2dOPb8s2M2M1')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+Hy12-MrRR2Y4ZGJl')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/TMV_TG')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/tmv_link/69')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/tmv_link/125')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', '❤️ Share And Support ❤️')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002037384930'))
